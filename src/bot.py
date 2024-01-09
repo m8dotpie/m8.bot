7e4715abdac43632bc52aac325298c1f2c1346c8
Igor Alentev <alentev.olm@ya.ru>
2024-01-09 10:29:23 +0000

    feat: admin channel status updates

--- src/bot.py
+++ src/bot.py
@@ -6,6 +6,7 @@ from starlette.routing import Route
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message, Update
 from dotenv import load_dotenv
+import datetime
 import telebot
 import aiohttp
 import asyncio
@@ -17,6 +18,7 @@ import os
 load_dotenv()
 
 secret_token_length = 20
+status_message_id = None
 API_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_HOST = os.getenv("WEBHOOK_DOMAIN")
 WEBHOOK_PORT = int(os.getenv("WEBHOOK_PORT"))
@@ -74,6 +76,26 @@ async def handle_start(message):
     )
 
 
+async def construct_vps_status():
+    cpu_usage = psutil.cpu_percent(interval=1)  # Get CPU usage as a percentage
+    memory_info = psutil.virtual_memory()
+    storage_info = psutil.disk_usage("/")
+    update_date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    response = (
+        f"CPU Usage: {cpu_usage}%\n"
+        f"Memory Usage:\n"
+        f"  - Total: {memory_info.total / (1024 ** 3):.2f} GB\n"
+        f"  - Used: {memory_info.used / (1024 ** 3):.2f} GB\n"
+        f"Storage Usage:\n"
+        f"  - Total: {storage_info.total / (1024 ** 3):.2f} GB\n"
+        f"  - Used: {storage_info.used / (1024 ** 3):.2f} GB\n"
+        f"Last updated: {update_date}"
+    )
+
+    return response
+
+
 @bot.message_handler(func=lambda message: message.text == "VPS Status")
 async def handle_vps_status(message):
     sent_message = await bot.send_message(message.chat.id, "Updating VPS status...")
@@ -82,19 +104,7 @@ async def handle_vps_status(message):
     # Remove the initial "VPS Status" message from the chat
     await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
 
-    cpu_usage = psutil.cpu_percent(interval=1)  # Get CPU usage as a percentage
-    memory_info = psutil.virtual_memory()
-    storage_info = psutil.disk_usage("/")
-
-    response = (
-        f"CPU Usage: {cpu_usage}%\n"
-        f"Memory Usage:\n"
-        f"  Total: {memory_info.total / (1024 ** 3):.2f} GB\n"
-        f"  Used: {memory_info.used / (1024 ** 3):.2f} GB\n"
-        f"Storage Usage:\n"
-        f"  Total: {storage_info.total / (1024 ** 3):.2f} GB\n"
-        f"  Used: {storage_info.used / (1024 ** 3):.2f} GB"
-    )
+    response = await construct_vps_status()
 
     await bot.edit_message_text(
         response, chat_id=message.chat.id, message_id=sent_message.message_id
@@ -125,10 +135,28 @@ async def startup() -> None:
         raise RuntimeError("unable to set webhook")
 
 
-async def run() -> None:
-    """Run the server."""
+async def start_status_service():
+    if status_message_id is None:
+        status_message_id = await bot.send_message(
+            ADMIN_CHANNEL_ID, "Starting status service..."
+        )
 
-    # Create the ASGI app
+    while True:
+        status = await construct_vps_status()
+        await bot.edit_message_text(
+            status, chat_id=ADMIN_CHANNEL_ID, message_id=status_message_id
+        )
+        await asyncio.sleep(60)
+
+
+async def run() -> None:
+    """Run the bot."""
+
+    # Start the status service
+    asyncio.create_task(start_status_service())
+
+    # WEBHOOK SERVER
+    # Create the ASGI app for webhook
     app = Starlette(
         routes=[
             Route("/webhook", telegram, methods=["POST"]),
@@ -136,8 +164,9 @@ async def run() -> None:
         on_startup=[startup],
     )
 
-    # Run the server
+    # Run the webhook server
     config = Config(app=app, host=WEBHOOK_LISTEN, port=WEBHOOK_PORT)
     server = Server(config)
+
     await server.serve()
     await bot.close_session()
